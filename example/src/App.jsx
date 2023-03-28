b48835cc1e2589d496ac8e8f4c99dab61bc5aa24
GalileoCap <galileocapp@gmail.com>
2023-03-28 20:26:00 -0300

    OK: Selecting which peer state

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,4 +1,7 @@
-import { useMeshContext } from 'react-peer-mesh';
+import {
+  useMeshContext,
+  MY_PEER, LEADER_PEER, ALL_PEERS,
+} from 'react-peer-mesh';
 
 function PeerState({ state }) {
   return (
@@ -12,16 +15,21 @@ function PeerState({ state }) {
 }
 
 export default function App() {
-  const [ myState, leaderState, ] = useMeshContext();
+  const myId = useMeshContext(MY_PEER, (peerState) => peerState._id);
+  const myState = useMeshContext(MY_PEER)
+  const leaderState = useMeshContext(LEADER_PEER);
+  const allStates = useMeshContext(ALL_PEERS);
 
   const onChange = () => window.setValue((draft) => { draft.number++ });
 
   return (
     <div className="App">
+      <p>My id: {myId}</p>
       <PeerState state={myState} />
       <PeerState state={leaderState} />
       <button onClick={onChange}>Change</button>
       <hr />
+      { allStates.map((peerState, idx) => <PeerState state={peerState} key={idx} />) }
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,10 @@ import {
 } from 'react';
 import { useImmer } from 'use-immer';
 
+export const MY_PEER = 0;
+export const LEADER_PEER = 1;
+export const ALL_PEERS = 2;
+
 const MeshContext = createContext(null);
 
 export function MeshProvider({ defaultValues, children }) {
@@ -38,6 +42,19 @@ export function MeshProvider({ defaultValues, children }) {
   );
 }
 
-export function useMeshContext() {
-  return useContext(MeshContext);
+function findPeer(peers, peerId) {
+  switch (peerId) {
+  case MY_PEER: return peers.find(peerState => peerState._mine);
+  case LEADER_PEER: return peers.find(peerState => peerState._leader);
+  case ALL_PEERS: return peers;
+  default: return peers.find(peerState => peerState._id === peerId);
+  }
+}
+
+export function useMeshContext(peerId, cb) {
+  const peers = useContext(MeshContext)
+  const res = findPeer(peers, peerId);
+
+  if (cb) return cb(res);
+  else return res;
 }
